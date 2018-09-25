f993c8f056f98a8d0fd4bea9406cb226ad789819
Reldalk <Reldalk@gmail.com>
2018-09-25 12:43:52 -0400

    higher order update

--- higherOrder.js
+++ higherOrder.js
@@ -1,6 +1,8 @@
 function repeat(fn, n){
-    for (let i = 0; i < n ; i++){
-        callback(fn);
+    for(let i = 0; i < n; i++){
+        fn("!");
     }
 }
 
+repeat(function hello(test){console.log("Hello World" + test)}, 5);
+repeat(function goodbye(test){console.log("Goodbye World" + test)}, 5);
